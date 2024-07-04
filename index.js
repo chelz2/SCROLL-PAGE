d1885a2835f8c5a574eceaf2fca2f7f24358d21e
chelz2 <golirida@hotmail.com>
2024-07-05 00:19:33 +0530

    modified index.js

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkBoxes);
 
-checkBoxes()
+checkBoxes();
 
 function checkBoxes(){
-    console.log(window.innerHeight)
+    const triggerBottom = window.innerHeight / 5 * 4
+    
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+        if(boxTop < triggerBottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    })
 }
